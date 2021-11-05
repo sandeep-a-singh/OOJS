bcd3d888a3102eac9c6e47de40601bdb55e7b251
sunny080593 <sunny9969888992@gmail.com>
2021-11-06 02:35:08 +0530

    Inheritance

--- object.js
+++ object.js
@@ -1,17 +1,37 @@
 function User(email, name) {
     this.email = email;
     this.name = name;
-    this.login = function(){
-        console.log(this.email, " ==> ", this.name);
+    this.online = false;
+    
+    this.print = function () {
+        console.log(this.name, this.online ? "Is LoggedIn" : "Is LoggedOut");
     }
 }
 
+User.prototype.login = function () {
+    this.online = true;
+    this.print();
+}
 
+User.prototype.logout = function () {
+    this.online = false;
+    this.print();
+}
+
+function Admin(...args) {
+    User.apply(this, args);
+    this.role = "Admin";
+}
+Admin.prototype = Object.create(User.prototype);
 
 var user = new User("s@gmail.com", "Sunny");
-var nextUser = new User("sandeep@gmail.com", "Sandeep");
-
+var admin = new Admin("sandeep@gmail.com", "Sandeep");
 
 console.log(user);
+console.log(admin);
 
-nextUser.login();
+user.login();
+user.logout();
+
+admin.login();
+admin.logout();
